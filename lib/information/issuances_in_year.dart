35942497812683b38df1c1e1c1fd4ee17039cfad
AE-GDY <74915033+AE-GDY@users.noreply.github.com>
2022-05-10 22:58:19 +0200

    Delete lib directory

--- lib/information/issuances_in_year.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class IssuancesInYear{
-  final double issuances;
-  final double year;
-
-  IssuancesInYear({Key? key, required this.issuances, required this.year});
-}
