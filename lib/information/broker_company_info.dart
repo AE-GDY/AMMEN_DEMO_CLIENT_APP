cce04430f2b512f9d8f803969ee7d66237899cba
AE-GDY <74915033+AE-GDY@users.noreply.github.com>
2022-05-12 13:45:38 +0200

    Delete lib directory

--- lib/information/broker_company_info.dart
+++
@@ -1,9 +0,0 @@
-import 'package:charts_flutter/flutter.dart' as charts;
-
-class BrokerCompany{
-  final int score;
-  final String name;
-  final charts.Color barColor;
-
-  BrokerCompany({required this.score, required this.name, required this.barColor});
-}
